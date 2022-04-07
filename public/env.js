4f5703b0c344558e6ae5110c004d3822d6e4f23d
CherryLiang <cherryliang0112@gmail.com>
2022-04-08 11:32:50 +1200

    feat: add sentry (#26)
    * add sentry
    
    * Add sentry dev key in pre release
    
    Co-authored-by: HZ <zhaihaohk@gmail.com>

--- public/env.js
+++ public/env.js
@@ -5,7 +5,8 @@ window.env = {
   // This option can be retrieved in "src/index.js" with "window.env.API_URL".
   NETWORK: 'testnet', // local | mainnet | testnet
   COORDINATOR_SERVICE_PORT: 8000,
-  COORDINATOR_SERVICE_URL: 'http://ec2-3-25-75-215.ap-southeast-2.compute.amazonaws.com:8000/graphql',
+  COORDINATOR_SERVICE_URL:
+    'http://ec2-3-25-83-26.ap-southeast-2.compute.amazonaws.com:8000/graphql',
   IPFS_GATEWAY: 'https://ipfs.thechainhub.com/api/v0',
   REGISTRY_PROJECT: 'https://api.subquery.network/sq/subquery/subquery-network-query-registry',
 };
