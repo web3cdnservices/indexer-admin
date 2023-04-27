847666334cd383cd95e65f19816e8a377facefa9
Lachlan McCrae <8554309+lamcc21@users.noreply.github.com>
2023-04-27 14:17:30 +1200

    Fix prompts, fix injectedConnector (#80)
    * Fix prompts, fix injectedConnector
    
    * Update prompts.ts
    
    ---------
    
    Co-authored-by: Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>

--- src/pages/metamask/prompts.ts
+++ src/pages/metamask/prompts.ts
@@ -6,23 +6,23 @@ import { Account } from 'pages/account/types';
 const prompts = (account: Account) => ({
   install: {
     title: 'Install MetaMask to use Indexer App',
-    desc: 'No MetaMask Extension found in the browser.  Please click the button to install the MetaMask Extension before connect with the network.',
+    desc: 'No MetaMask Extension found in the browser. Please click the button to install the MetaMask Extension before connecting to the network.',
     buttonTitle: 'Install MetaMask in the extension market',
   },
   connect: {
     title: 'Connect wallet to use Indexer App',
-    desc: 'Use the indexer app to connect with Subquery network, you can manage you accounts and projects inside the app.',
+    desc: 'Use the indexer app to connect with the Subquery network. You can manage your accounts and projects inside the app.',
     buttonTitle: 'Connect with MetaMask browser extension',
   },
   invalidAccount: {
     title: 'Incorrect connected account with coordinator service',
-    desc: `Please switch the connect account to ${account}`,
+    desc: `Please switch the connected account to ${account}`,
     buttonTitle: 'Switch account to use the admin app',
   },
   invalidNetwork: {
-    title: 'Unsupportted network Type',
-    desc: 'MetaMask connect with an unsupported network, please press the button to swith to the correct network',
-    buttonTitle: 'Swtich to the supportted network',
+    title: 'Unsupported network type',
+    desc: 'MetaMask is connected to an unsupported network. Please press the button to switch to the correct network.',
+    buttonTitle: 'Switch to the supported network',
   },
 });
 
