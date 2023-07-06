a5cc8931291237fbb9ebd437f6485ebeb467a902
Cyrbuzz <cyrbuzz@foxmail.com>
2023-07-06 09:52:40 +0800

    fix: use relative url (#110)

--- public/env.js
+++ public/env.js
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // docker will override this environment.
+// define at coordinator project.
 window.env = {
-  // This option can be retrieved in "src/index.js" with "window.env.API_URL".
+  // COORDINATOR_SERVICE_PORT not using now.
   COORDINATOR_SERVICE_PORT: 8000,
   COORDINATOR_SERVICE_URL:
     'http://ec2-13-238-217-178.ap-southeast-2.compute.amazonaws.com:8000/graphql',
